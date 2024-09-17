c7f3ed896a590f512af9f91dd95e3fb2e24de689
dheerendra singh <bhandaridheere@gmail.com>
2024-09-17 09:18:18 +0530

    add

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use('/api', newsRoute)
+app.get('/', (req, res) => {
+    res.send('hello this is news page')
+})
 
 mongoose.connect(process.env.MONGO_URI,)
     .then(() => {
